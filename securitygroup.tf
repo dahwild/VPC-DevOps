fd4101c86c039b05ca377ee3504658bcb7cd3ec9
dahwild <dahwild@gmail.com>
2020-08-05 13:55:42 -0500

    Allow port 80

--- securitygroup.tf
+++ securitygroup.tf
@@ -19,19 +19,19 @@ resource "aws_security_group" "allow-ssh" {
     Name = "allow-ssh"
   }
 
-# ingress {
-#   from_port   = 80
-#   to_port     = 80
-#   protocol    = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-# }
-#
-# egress {
-#   from_port   = 80
-#   to_port     = 80
-#   protocol    = "tcp"
-#   cidr_blocks = ["0.0.0.0/0"]
-# }
+ ingress {
+   from_port   = 80
+   to_port     = 80
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+ egress {
+   from_port   = 80
+   to_port     = 80
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
 
 }
 
