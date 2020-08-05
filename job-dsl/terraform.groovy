5bf93d050cb9e7c878062cf55a3336765707e9de
dahwild <dahwild@gmail.com>
2020-08-05 12:10:26 -0500

    update owner and credentials ID

--- job-dsl/terraform.groovy
+++ job-dsl/terraform.groovy
@@ -2,7 +2,7 @@ multibranchPipelineJob('TerraformPipeline') {
     branchSources {
         github {
             id('dba5c7ce-b56d-439c-8476-3d938624049c') // IMPORTANT: use a constant and unique identifier
-            scanCredentialsId('github-ci')
+            scanCredentialsId('dba5c7ce-b56d-439c-8476-3d938624049c')
             repoOwner('dahwild')
             repository('VPC-DevOps')
         }
